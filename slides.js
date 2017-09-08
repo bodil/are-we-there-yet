b1ce501d684b9953eb3b5fc57cfa984bd8ffc3f3
Bodil Stokke <bodil@bodil.org>
2017-09-09 00:41:41 +0100

    Make it so.

---
+++ slides.js
@@ -0,0 +1,8 @@
+import Pink from "pink";
+
+import "pink/css/themes/league.less";
+
+new Pink("#slides", {
+  "background": require("pink/modules/background"),
+  "image": require("pink/modules/image")
+});
